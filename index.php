8e940e2ac946e6e54a837958376095d1cfb6736b
chenlei <chenlei@haitun.hk>
2020-11-18 20:01:30 +0800

    修改服务端端口

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$client = new \Face_recog_package\FaceRecogPackage('127.0.0.1:50052', [
+$client = new \Face_recog_package\FaceRecogPackage('127.0.0.1:8787', [
     'credentials' => \Grpc\ChannelCredentials::createInsecure()
 ]);
 $request = new \Face_recog_package\FaceServDetectRequest();
