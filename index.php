d761743d0217432698694700e5eadec79d6d3fa7
chenlei <chenlei@haitun.hk>
2020-11-20 14:36:55 +0800

    修改

--- index.php
+++ index.php
@@ -5,10 +5,15 @@ $client = new \Face\FaceRecogPackageClient('127.0.0.1:8787', [
     'credentials' => \Grpc\ChannelCredentials::createInsecure()
 ]);
 
-$request = new \Face\FaceServDetectRequest();
-$request->setImage('s');
-$request->setUserID('1');
-$request->setMessage('test');
+$re = new \stdClass();
+$re->userId = 1;
+$re->mesage = '';
+$re->image = '';
+
+$request = new \Face\FaceServDetectRequest($re);
+//$request->setImage('s');
+//$request->setUserID('1');
+//$request->setMessage('test');
 $get = $client->FaceServDetct($request)->wait();
 list($reply, $status) = $get;
 $data = $reply->getMessage();
