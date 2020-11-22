7f720846cbaa7d42c5c71d7edbb378bf79d9fedf
chenlei <chenlei@haitun.hk>
2020-11-22 17:14:25 +0800

    新增

--- Face_recog_package/FaceRecogPackageClient.php
+++ Face_recog_package/FaceRecogPackageClient.php
@@ -30,4 +30,25 @@ class FaceRecogPackageClient extends \Grpc\BaseStub{
             $metadata, $options);
     }
 
+
+    public function FaceServRegister(\Face_recog_package\FaceServRequestMsg $argument, $metadata=[], $options=[]){
+        // (/FaceRecogService/FaceServDetct) 是请求服务端那个服务和方法，基本和 proto 文件定义一样
+        // (\Face_recog_package\FaceServ1VS1Result) 是响应信息（那个类），基本和 proto 文件定义一样
+        return $this->_simpleRequest('/face_recog_package.FaceRecogService/FaceServRegister',
+            $argument,
+            ['\Face_recog_package\FaceServReplyMsg', 'decode'],
+            $metadata, $options);
+    }
+
+    public function FaceServ1VSN(\Face_recog_package\FaceServRequestMsg $argument, $metadata=[], $options=[]){
+        // (/FaceRecogService/FaceServDetct) 是请求服务端那个服务和方法，基本和 proto 文件定义一样
+        // (\Face_recog_package\FaceServ1VS1Result) 是响应信息（那个类），基本和 proto 文件定义一样
+        return $this->_simpleRequest('/face_recog_package.FaceRecogService/FaceServ1VSN',
+            $argument,
+            ['\Face_recog_package\FaceServReplyMsg', 'decode'],
+            $metadata, $options);
+    }
+
+
+
 }
--- index-face.php
+++ index-face.php
@@ -4,7 +4,26 @@ $client = new \Face_recog_package\FaceRecogPackageClient('127.0.0.1:8787', [
     'credentials' => \Grpc\ChannelCredentials::createInsecure()
 ]);
 
+// FaceServDetect
+echo "FaceServDetect :";
+$request = new \Face_recog_package\FaceServDetectRequest();
+$request->setUserID(2);
+$request->setMessage('test');
+$request->setImage('2341234');
+$requestMsg = new \Face_recog_package\FaceServRequestMsg();
+$requestMsg->setType('1');
+$requestMsg->setReqDet($request);
+$get = $client->FaceServDetect($requestMsg)->wait();
+print_r($get);
+list($reply, $status) = $get;
+$data['type'] = $reply->getType();
+$data['message'] = $reply->getRetDet()->getMessage();
+$data['retCode'] = $reply->getRetDet()->getRetCode();
+$data['userId'] = $reply->getRetDet()->getUserID();
+var_dump($data);
 
+// FaceServ1VS1
+echo "FaceServ1VS1 :";
 $request = new \Face_recog_package\FaceServ1VS1Request();
 $request->setUserID(2);
 $request->setMessage('test');
@@ -20,7 +39,51 @@ $data['type'] = $reply->getType();
 $data['message'] = $reply->getRet1Vs1()->getMessage();
 $data['retCode'] = $reply->getRet1Vs1()->getRetCode();
 $data['userId'] = $reply->getRet1Vs1()->getUserID();
-var_dump($data);die;
+var_dump($data);
+
+
+
+// FaceServRegister
+echo "FaceServRegister :";
+$request = new \Face_recog_package\FaceServ1VS1Request();
+$request->setUserID(2);
+$request->setMessage('test');
+$request->setImage1('2341234');
+$request->setImage2('2341234');
+$requestMsg = new \Face_recog_package\FaceServRequestMsg();
+$requestMsg->setType('3');
+$requestMsg->setReq1Vs1($request);
+$get = $client->FaceServRegister($requestMsg)->wait();
+print_r($get);
+list($reply, $status) = $get;
+$data['type'] = $reply->getType();
+$data['message'] = $reply->getRetRegister()->getMessage();
+$data['retCode'] = $reply->getRetRegister()->getRetCode();
+$data['userId'] = $reply->getRetRegister()->getUserID();
+var_dump($data);
+
+// FaceServ1VSN
+echo "FaceServ1VSN :";
+$request = new \Face_recog_package\FaceServ1VSNRequest();
+$request->setUserID(2);
+$request->setMessage('test');
+$request->setImage('2341234');
+$request->setGroupID(2);
+$requestMsg = new \Face_recog_package\FaceServRequestMsg();
+$requestMsg->setType('4');
+$requestMsg->setReq1VsN($request);
+$get = $client->FaceServ1VSN($requestMsg)->wait();
+print_r($get);
+list($reply, $status) = $get;
+$data['type'] = $reply->getType();
+$data['message'] = $reply->getRet1VsN()->getMessage();
+$data['retCode'] = $reply->getRet1VsN()->getRetCode();
+$data['userId'] = $reply->getRet1VsN()->getUserID();
+$data['groupID'] = $reply->getRet1VsN()->getGroupID();
+var_dump($data);
+
+
+die;
 
 
 
